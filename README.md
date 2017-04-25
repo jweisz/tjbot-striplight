1efa15a708ba3d63704e64027cffcc8b59637232
Justin Weisz <jweisz@us.ibm.com>
2017-04-25 15:53:33 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # tjbot-striplight
-Adds support for a NeoPixel RGB LED strip light to TJBot
+
+> Adds support for a NeoPixel RGB LED strip light to TJBot
+
+This is an extension of the [TJBot library](http://github.com/ibmtjbot/tjbotlib) that adds support for a NeoPixel RGB LED strip light.
+
+# Usage
+
+This is a drop-in replacement for the `tjbot` library. 
+
+First, clone this repository.
+
+```
+cd ~/Desktop
+git clone https://github.com/jweisz/tjbot-striplight
+```
+
+Next, include this library in your recipe and instantiate the `TJBot` object with the `led-strip` hardware.
+
+```
+const TJBot = require(‘/home/pi/Desktop/tjbot-striplight’);
+
+var hardware = [‘led-strip’];
+var configuration = {
+    shine: {
+        led_strip: {
+            num_leds: 60
+        }
+    }
+};
+
+var tj = new TJBot(hardware, configuration, {});
+tj._setupLEDStrip();
+```
+
+> Note: TJBot does not support the simultaneous usage of an `led` and an `led-strip`. If TJBot is initialized with both pieces of hardware, it will throw an error.
+
+> Note: You must call `tj._setupLEDStrip()` manually after constructing the TJBot object to initalize the new hardware.
+
+# New API Methods
+
+### tj.shineStrip(rgbColors)
+
+Shines the LED strip with the given `Uint32Array` of colors.
+
+### tj.rainbowStrip(offset)
+
+Shines the LED strip with a rainbow pattern, starting with the given `offset`. An `offset` of 0 will begin the rainbow at the first LED. An `offset` of 5 will begin the rainbow at the 5th LED.
+
+### tj.shineStripWithRGBColor(color)
+
+Shines all LEDs in the strip the given RGB color. This color must be interpretable by `tj._normalizeColor()`, which means named colors (e.g. “red”, “blue”, “orange”) and colors specified in hexadecimal (e.g. “#F2A7C1”) are valid options.
+
+### tj.shineStripWithHSLColor(h, s, l)
+
+Shines all LEDs in the strip the given HSL color. Saturation and lightness values of 0.5 seem to work well for the NeoPixel LEDs.
+
+# Contributing
+We encourage you to make enhancements to this library and contribute them back to us via a pull request.
+
+# License
+This project uses the [Apache License Version 2.0](LICENSE) software license.
---
+++ tjbot-striplight.js
@@ -0,0 +1,196 @@
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var TJBot = require('tjbot');
+
+/** ------------------------------------------------------------------------ */
+/** CONFIGURATION                                                            */
+/** ------------------------------------------------------------------------ */
+
+// overlay new hardware for the striplight
+TJBot.prototype.hardware.push('led-strip');
+
+TJBot.prototype.defaultConfiguration.shine = {
+    led_strip: {
+        num_leds: 60
+    }
+}
+
+/** ------------------------------------------------------------------------ */
+/** LED-STRIP                                                                */
+/** ------------------------------------------------------------------------ */
+
+/**
+ * Set up the LED strip light. Must be called manually after calling the TJBot constructor.
+ */
+TJBot.prototype._setupLEDStrip = function() {
+    // make sure the tjbot doesn't have an led already configured
+    if (this.hardware.includes('led')) {
+        throw new Error("TJBot doesn't support both an led and led-strip, please include only one in the hardware configuration.");
+    }
+    
+    var ws281x = require('rpi-ws281x-native');
+    tj._ledstrip = ws281x;
+    tj._ledstrip.init(this.configuration.shine.led_strip.num_leds);
+    
+    // clean up the led strip before the process exits
+    process.on('SIGINT', function() {
+        tj._ledstrip.reset();
+        process.nextTick(function() {
+            process.exit(0);
+        })
+    });
+}
+
+/**
+ * Assert that the TJBot is configured with an LED strip.
+ */
+TJBot.prototype._assertLEDStrip = function() {
+    if (!this._ledstrip) {
+        throw new Error(
+            'TJBot is not configured to led-strip. ' +
+            'Please check that you included the "led-strip" hardware in the TJBot constructor.');
+    }
+}
+
+/**
+ * Convert a color from HSL space to RGB space.
+ *
+ * @param {Double} h The hue of the HSL color.
+ * @param {Double} s The saturation of the HSL color.
+ * @param {Double} l The lightness of the HSL color.
+ */
+TJBot.prototype._hslToRgb = function(h, s, l) {
+    var r, g, b;
+    
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        var hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1/6) return p + (q - p) * 6 * t;
+            if (t < 1/2) return q;
+            if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+        
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+    
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+/**
+ * Shine the LED strip to the given colors.
+ *
+ * @param {Uint32Array} rgbColors The array of RGB colors.
+ */
+TJBot.prototype.shineStrip = function(rgbColors) {
+    this._assertLEDStrip();
+    
+    if (!colors.constructor === Array) {
+        throw new Error("shineStrip() expects a Uint32Array of colors");
+    }
+    
+    if (!colors.length != this.configuration.shine.led_strip.num_leds) {
+        throw new Error("shineStrip() expects an array with " + this.configuration.shine.led_strip.num_leds + " elements");
+    }
+    
+    // render
+    winston.verbose("TJBot shining LED strip to " + rgbColors.join(','));
+    this._ledstrip.render(rgbColors);
+}
+
+/**
+ * Shine the LED strip to a rainbow pattern, with a given offset.
+ *
+ * @param {Integer} offset The offset of the pattern; an offset of 0 begins at red and ends at blue, an offset will shift where the rainbow begins.
+ */
+TJBot.prototype.rainbowStrip = function(offset) {
+    this._assertLEDStrip();
+    
+    var numLED = this.configuration.shine.led_strip.num_leds;
+    var hueStep = 1 / numLED;
+    var colors = new Uint32Array(numLED);
+    
+    for (var i = 0; i < numLED; i++) {
+        var h = i * hueStep;
+        var s = 0.5;
+        var l = 0.5;
+        var rgb = this._hslToRgb(h, s, l);
+        var grb_int = rgb[1] << 0x10 | rgb[0] << 0x08 | rgb[2];
+        colors[i] = grb_int;
+    }
+    
+    // rotate by offset
+    for (var i = 0; i < offset; i++) {
+        colors.unshift(colors.pop());
+    }
+    
+    this.shineStrip(colors);
+}
+
+/**
+ * Shine the LED strip to a single RGB color.
+ *
+ * @param {String} color The color to use. Must be interpretable by TJBot.prototype._normalizeColor.
+ */
+TJBot.prototype.shineStripWithRGBColor = function(color) {
+    this._assertLEDStrip();
+
+    // convert to rgb
+    var rgb = this._normalizeColor(color);
+
+    // convert hex to the 0xGGRRBB format for the LED
+    var grb = "0x" + rgb[3] + rgb[4] + rgb[1] + rgb[2] + rgb[5] + rgb[6];
+
+    // determine the LED colors
+    var colors = new Uint32Array(this.configuration.shine.led_strip.num_leds);
+    for (var i = 0; i < this.configuration.shine.led_strip.num_leds; i++) {
+        colors[i] = parseInt(grb);
+    }
+    
+    // shine
+    this.shineStrip(colors);
+}
+
+/**
+ * Shine the LED strip to a single HSL color.
+ *
+ * @param {Double} h The hue of the HSL color.
+ * @param {Double} s The saturation of the HSL color.
+ * @param {Double} l The lightness of the HSL color.
+ */
+TJBot.prototype.shineStripWithHSLColor = function(h, s, l) {
+    this._assertLEDStrip();
+
+    // convert to rgb
+    var rgb = this._hslToRgb(h, s, l);
+    var grb_int = rgb[1] << 0x10 | rgb[0] << 0x08 | rgb[2];
+    
+    // determine the LED colors
+    var colors = new Uint32Array(this.configuration.shine.led_strip.num_leds);
+    colors.fill(grb_int);
+    
+    this.shineStrip(colors);
+}
+
+module.exports = TJBot;
